0b2518248a779934f02925473f35fee2a5c6702a
Cianuro-83 <buzzeosimone@yahoo.com>
2023-02-09 15:53:27 +0100

    FEAT: play button e ciclo for griglia in js

---
+++ js/myscript.js
@@ -0,0 +1,27 @@
+console.log("Ciao Cianuro... oggi si gioca a CAMPO MINATO");
+// .....................................................................................................RICHIAMO ELEMENTI DAL DOM
+// .....................................................................................................
+const grigliaElement = document.getElementById("griglia");
+const playBtnElement = document.getElementById("play-button");
+console.log(grigliaElement, playBtnElement);
+
+// .....................................................................................................IMPOSTO LE DIMENSIONI DELLA GRIGLIA
+// .....................................................................................................
+let gridSize = 10;
+let griCell = gridSize * gridSize;
+
+// .....................................................................................................APERTURA addEventListener su BOTTONE PLAY
+// .....................................................................................................
+playBtnElement.addEventListener("click", function () {
+  console.log("INIZIO GIOCO");
+
+  // .....................................................................................................CICLO LE CELLE CON FOR PER CREARE LA GRIGLIA SUL DOM
+  // ..............................................................................
+  for (let i = 0; i < griCell; i++) {
+    let index = i + 1;
+    console.log(index);
+    // CHIUSURA CICLO FOR
+  }
+  // ...................................................................................................CHIUSURA addEventListener su BOTTONE PLAY
+  // .....................................................................................................
+});
